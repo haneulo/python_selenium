0464178f5e0fc0c02af1d53757596b41c36ae140
Oh Haneul <hneuloh@gmail.com>
2025-03-23 14:35:08 +0900

    Naver News Crawling - Add scroll behavior

--- selenium/news_search/app.py
+++ selenium/news_search/app.py
@@ -14,12 +14,19 @@ search_link = f"https://search.naver.com/search.naver?where=news&ie=utf8&sm=nws_
 
 driver.get(search_link)
 
-time.sleep(0.5)
+time.sleep(1)
 
 # 최신순으로 보기
 Latest_selector = "#snb > div.mod_group_option_filter._search_option_simple_wrap > div > div.option_area.type_sort > a:nth-child(2)"
 Latest_element = driver.find_element(By.CSS_SELECTOR, Latest_selector)
 Latest_element.click()
+
+time.sleep(1)
+
+# 스크롤 동작(스크롤 1번당 10개의 뉴스 추가)
+for _ in range(1):
+    driver.execute_script("window.scrollBy(0,10000);")
+    time.sleep(1)
 
 req = driver.page_source
 soup = BeautifulSoup(req, 'html.parser')
