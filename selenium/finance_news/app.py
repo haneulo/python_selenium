1bd83cea8c6a3b4102cfce1851b9a0c8b95178f7
Oh Haneul <hneuloh@gmail.com>
2025-04-05 14:28:05 +0900

    Naver Finance News Crawling - Find news articles up to the last page

--- selenium/finance_news/app.py
+++ selenium/finance_news/app.py
@@ -14,20 +14,25 @@ chrome_options.add_experimental_option("excludeSwitches", ["enable-logging"])
 
 driver = webdriver.Chrome(options=chrome_options)
 
-search_date = "2025-04-01"
-search_link = f"https://finance.naver.com/news/mainnews.naver?date={search_date}"
+for i in range(1, 1000):
+    dt = "2025-04-05"
+    search_link = f"https://finance.naver.com/news/mainnews.naver?date={dt}&page={i}"
 
-driver.get(search_link)
+    driver.get(search_link)
 
-req = driver.page_source
-soup = BeautifulSoup(req, 'html.parser')
+    req = driver.page_source
+    soup = BeautifulSoup(req, 'html.parser')
 
-articles = soup.select(".block1")
-for article in articles:
-    title = article.select_one(".articleSubject > a").text
-    url = 'https://finance.naver.com/' + article.select_one(".articleSubject > a").attrs['href']
-    content = article.select_one(".articleSummary").contents[0].strip()
-    press = article.select_one(".press").text.strip()
-    date = article.select_one(".wdate").text
-    print(title, url, content, press, date)
+    articles = soup.select(".block1")
+    for article in articles:
+        title = article.select_one(".articleSubject > a").text
+        url = 'https://finance.naver.com/' + article.select_one(".articleSubject > a").attrs['href']
+        content = article.select_one(".articleSummary").contents[0].strip()
+        press = article.select_one(".press").text.strip()
+        date = article.select_one(".wdate").text
+        print(title, url, content, press, date)
+
+    # 마지막 페이지 체크
+    if soup.select_one(".pgRR") == None:
+        break
 
